4b6145809d6663c3db4e905fd18f804582f35934
Abdelhakim OUBRAHAM <abdel.oubraham@gmail.com>
2021-12-10 00:28:01 +0100

    Define Error to be used with validation

---
+++ Funzionale/Error/Error.cs
@@ -0,0 +1,36 @@
+﻿namespace Funzionale
+{
+    using static Prelude;
+
+    public readonly struct Error
+    {
+        public Option<string> Message { get; init; }
+        public Option<Exception> Exception { get; init; }
+        public Option<Error> Inner => from e in inner select e.ToError();
+        
+        internal Option<InnerError> inner { get; init; }
+
+        internal InnerError ToInnerError() => new()
+        {
+            Message = Message,
+            Exception = Exception,
+            Inner = inner,
+        };
+
+        public static implicit operator Error(string message) => new() { Message = some(message) };
+
+        public static implicit operator Error(Exception exception) => new() { Exception = some(exception) };
+
+        public static implicit operator Error((string message, Exception exception) info) => new()
+        {
+            Message = some(info.message),
+            Exception = some(info.exception),
+        };
+
+        public static implicit operator Error((string message, Error inner) info) => new()
+        {
+            Message = some(info.message),
+            inner = some(info.inner.ToInnerError()),
+        };
+    }
+}
---
+++ Funzionale/Error/InnerError.cs
@@ -0,0 +1,20 @@
+﻿namespace Funzionale
+{
+    /// <summary>
+    /// This is intended to be used to avoid cycle errors in the Error
+    /// struct when nesting errors.
+    /// </summary>
+    internal class InnerError
+    {
+        public Option<string> Message { get; init; }
+        public Option<Exception> Exception { get; init; }
+        public Option<InnerError> Inner { get; init; }
+
+        public Error ToError() => new()
+        {
+            Message = Message,
+            Exception = Exception,
+            inner = Inner,
+        };
+    }
+}
