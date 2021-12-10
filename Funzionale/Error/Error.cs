e2b5a71412cd353f0244cd9ff3107c45dd59ddff
Abdelhakim OUBRAHAM <abdel.oubraham@gmail.com>
2021-12-10 20:01:49 +0100

    Refactor Error

--- Funzionale/Error/Error.cs
+++ Funzionale/Error/Error.cs
@@ -6,8 +6,12 @@
     {
         public Option<string> Message { get; init; }
         public Option<Exception> Exception { get; init; }
-        public Option<Error> Inner => from e in inner select e.ToError();
-        
+        public Option<Error> Inner
+        {
+            get => from i in inner select i.ToError();
+            init => inner = from e in value select e.ToInnerError();
+        }
+
         internal Option<InnerError> inner { get; init; }
 
         internal InnerError ToInnerError() => new()
@@ -18,7 +22,6 @@
         };
 
         public static implicit operator Error(string message) => new() { Message = some(message) };
-
         public static implicit operator Error(Exception exception) => new() { Exception = some(exception) };
 
         public static implicit operator Error((string message, Exception exception) info) => new()
@@ -31,6 +34,24 @@
         {
             Message = some(info.message),
             inner = some(info.inner.ToInnerError()),
+        };
+    }
+
+    /// <summary>
+    /// This is intended to be used to avoid cycle errors in the Error
+    /// struct when nesting errors.
+    /// </summary>
+    internal sealed class InnerError
+    {
+        public Option<string> Message { get; init; }
+        public Option<Exception> Exception { get; init; }
+        public Option<InnerError> Inner { get; init; }
+
+        public Error ToError() => new()
+        {
+            Message = Message,
+            Exception = Exception,
+            inner = Inner,
         };
     }
 }
