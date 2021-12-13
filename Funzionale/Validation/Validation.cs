cc7dd1e8411908b1874ff84850acfee07682272e
Abdelhakim OUBRAHAM <abdel.oubraham@gmail.com>
2021-12-13 13:13:46 +0100

    Add validation

---
+++ Funzionale/Validation/Validation.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Funzionale
+{
+    using static Prelude;
+    using Validation;
+    public static partial class Prelude
+    {
+        public static Validation<T> valid<T>([DisallowNull][NotNull] T t) => new(t);
+
+        public static Invalid invalid([DisallowNull][NotNull] IEnumerable<Error> errors) => new(errors);
+        public static Invalid invalid([DisallowNull][NotNull] params Error[] errors) => new(errors);
+
+        public static Validation<T> invalid<T>([DisallowNull][NotNull] IEnumerable<Error> errors) => invalid(errors);
+        public static Validation<T> invalid<T>([DisallowNull][NotNull] params Error[] errors) => invalid(errors);
+    }
+
+    namespace Validation
+    {
+        public readonly struct Invalid
+        {
+            internal readonly IEnumerable<Error> Errors;
+            internal Invalid([DisallowNull][NotNull] IEnumerable<Error> errors)
+            {
+                if (errors is null)
+                    throw new ArgumentNullException(nameof(errors), "Cannot initialize the invalid state with a null collection of errors. Consider providing an empty Collection.");
+                Errors = errors;
+            }
+        }
+    }
+
+    public readonly struct Validation<T>
+    {
+        private readonly IEnumerable<Error> errors;
+        private readonly T? value;
+
+        [MemberNotNullWhen(true, nameof(value))]
+        private bool isValid { get; }
+
+        internal Validation([DisallowNull][NotNull] T t)
+        {
+            if (t is null)
+                throw new ArgumentNullException(nameof(t), "Cannot initialize a Validation in the valid state with a null value. Consider using an Option instead.");
+            errors = Enumerable.Empty<Error>();
+            value = t;
+            isValid = true;
+        }
+
+        internal Validation([DisallowNull][NotNull] IEnumerable<Error> errors)
+        {
+            if (errors is null)
+                throw new ArgumentNullException(nameof(errors), "Cannot initialize a Validation in the invalid state with a null collection of errors. Consider providing an empty Collection.");
+            this.errors = errors;
+            value = default;
+            isValid = false;
+        }
+
+        public static implicit operator Validation<T>(Invalid invalid) => new(invalid.Errors);
+        public static implicit operator Validation<T>(Error error) => invalid(error);
+        public static implicit operator Validation<T>([DisallowNull][NotNull] T t) => new(t);
+
+        public R Match<R>(Func<IEnumerable<Error>, R> Invalid, Func<T, R> Valid) => isValid ? Valid(value) : Invalid(errors);
+    }
+}
