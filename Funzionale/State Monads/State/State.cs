8f8827977f875da4a50ef166db6460e2981e8c93
Abdelhakim OUBRAHAM <abdel.oubraham@gmail.com>
2021-12-15 16:30:38 +0100

    Define state monad

---
+++ Funzionale/State Monads/State/State.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Funzionale
+{
+    using static Prelude;
+    public static partial class Prelude
+    {
+        public static State<S, T> state<S, T>([DisallowNull][NotNull] T value) => new();
+    }
+
+    /// <summary>
+    /// A monad that produces a value T and changes state along the computation
+    /// </summary>
+    public readonly struct State<S, T>
+    {
+        internal readonly Func<S, (T value, S state)> func;
+        internal State([DisallowNull][NotNull] Func<S, (T value, S state)> f) =>
+            func = f ?? throw new ArgumentNullException(nameof(f), "Cannot initialize a State monad with an empty delegate");
+
+        public static implicit operator State<S, T>([DisallowNull][NotNull] Func<S, (T value, S state)> f) => new(f);
+
+        public (Exceptional<T> value, S state) Run(S state)
+        {
+            try
+            {
+                if (state is null)
+                    throw new ArgumentNullException(nameof(state), "The initiale state provided to the State monad was null. Consider using an Option instead.");
+
+                var (value, newState) = func(state);
+
+                if (value is null)
+                    throw new ArgumentNullException(nameof(value), "The function provided to the Writer returned a null value. Consider using an Option instead.");
+
+                if (newState is null)
+                    throw new ArgumentNullException(nameof(value), "The function provided to the Writer returned a null aggregate. Consider using an Option instead.");
+
+                return (success(value!), newState);
+            }
+            catch (Exception ex)
+            {
+                return (failure<T>(ex), state);
+            }
+        }
+    }
+}
---
+++ Funzionale/State Monads/State/StateExtensions.cs
@@ -0,0 +1,59 @@
+﻿namespace Funzionale
+{
+    public static partial class StateExtensions
+    {
+        // Functor
+
+        public static State<S, R> Map<T, R, S>(this State<S, T> @this, Func<T, R> map) =>
+            new(state => @this.func(state).Transform(x => (map(x.value), x.state)));
+
+        public static State<S, Func<T2, R>> Map<T1, T2, R, S>(this State<S, T1> @this, Func<T1, T2, R> map) =>
+            @this.Map(map.Curry());
+
+        public static State<S, Func<T2, T3, R>> Map<T1, T2, T3, R, S>(this State<S, T1> @this, Func<T1, T2, T3, R> map) =>
+            @this.Map(map.CurryFirst());
+
+        public static State<S, Func<T2, T3, T4, R>> Map<T1, T2, T3, T4, R, S>(this State<S, T1> @this, Func<T1, T2, T3, T4, R> map) =>
+            @this.Map(map.CurryFirst());
+
+        public static State<S, Func<T2, T3, T4, T5, R>> Map<T1, T2, T3, T4, T5, R, S>(this State<S, T1> @this, Func<T1, T2, T3, T4, T5, R> map) =>
+            @this.Map(map.CurryFirst());
+
+        // Monad
+
+        public static State<S, R> Bind<T, R, S>(this State<S, T> @this, Func<T, State<S, R>> bind) =>
+            new(state => @this.func(state).Transform(x => bind(x.value).func(x.state)));
+
+        // Applicative
+
+        public static State<S, R> Apply<T, R, S>(this State<S, Func<T, R>> @this, State<S, T> arg) =>
+            new(state => @this.func(state)
+                .Transform(x => arg.func(x.state)
+                    .Transform(y => (x.value(y.value), y.state))));
+
+        public static State<S, Func<T2, R>> Apply<T1, T2, R, S>(this State<S, Func<T1, T2, R>> @this, State<S, T1> arg) =>
+            @this.Map(FuncExtensions.Curry).Apply(arg);
+
+        public static State<S, Func<T2, T3, R>> Apply<T1, T2, T3, R, S>(this State<S, Func<T1, T2, T3, R>> @this, State<S, T1> arg) =>
+            @this.Map(FuncExtensions.CurryFirst).Apply(arg);
+
+        public static State<S, Func<T2, T3, T4, R>> Apply<T1, T2, T3, T4, R, S>(this State<S, Func<T1, T2, T3, T4, R>> @this, State<S, T1> arg) =>
+            @this.Map(FuncExtensions.CurryFirst).Apply(arg);
+
+        public static State<S, Func<T2, T3, T4, T5, R>> Apply<T1, T2, T3, T4, T5, R, S>(this State<S, Func<T1, T2, T3, T4, T5, R>> @this, State<S, T1> arg) =>
+            @this.Map(FuncExtensions.CurryFirst).Apply(arg);
+
+        // Utilities
+
+        public static State<S, Unit> ForEach<T, S>(this State<S, T> @this, Action<T> action) => @this.Map(action.ToFunc());
+
+        // LINQ
+
+        public static State<S, R> Select<T, R, S>(this State<S, T> @this, Func<T, R> map) => @this.Map(map);
+
+        public static State<S, R> SelectMany<T, R, S>(this State<S, T> @this, Func<T, State<S, R>> bind) => @this.Bind(bind);
+
+        public static State<S, V> SelectMany<T, R, V, S>(this State<S, T> @this, Func<T, State<S, R>> bind, Func<T, R, V> project) =>
+                @this.Bind(t => bind(t).Map(r => project(t, r)));
+    }
+}
