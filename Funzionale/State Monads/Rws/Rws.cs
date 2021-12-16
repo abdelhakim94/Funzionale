fc8c6e7cbc6566f12a101554a3ca41fc393f6240
Abdelhakim OUBRAHAM <abdel.oubraham@gmail.com>
2021-12-16 09:57:36 +0100

    Define Rws

---
+++ Funzionale/State Monads/Rws/Rws.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Funzionale
+{
+    using static Prelude;
+
+    public static partial class Prelude
+    {
+    }
+
+    public readonly struct Rws<MonoidW, Env, W, S, T> where MonoidW : struct, Monoid<W>
+    {
+        internal readonly Func<Env, S, (T value, W output, S state)> func;
+
+        internal Rws([DisallowNull][NotNull] Func<Env, S, (T value, W output, S state)> f) =>
+            func = f ?? throw new ArgumentNullException(nameof(f), "Cannot initialize an Rws with an empty delegate.");
+
+        public static implicit operator Rws<MonoidW, Env, W, S, T>(Func<Env, S, (T value, W output, S state)> f) => new(f);
+
+        public (Exceptional<T> value, W output, S state) Run(Env env, S state)
+        {
+            try
+            {
+                if (state is null)
+                    throw new ArgumentNullException(nameof(state), "The initiale state provided to the Rws monad was null. Consider using an Option instead.");
+
+                var (value, output, newState) = func(env, state);
+
+                if (value is null)
+                    throw new ArgumentNullException(nameof(func), "The function provided to the Rws returned null. Consider using an Option.");
+
+                if (output is null)
+                    throw new ArgumentNullException(nameof(value), "The function provided to the Writer returned a null aggregate. Consider using an Option instead.");
+
+                if (newState is null)
+                    throw new ArgumentNullException(nameof(value), "The function provided to the Writer returned a null aggregate. Consider using an Option instead.");
+
+                return (success(value), output, newState);
+            }
+            catch (Exception ex)
+            {
+                return (failure<T>(ex), default(MonoidW).Empty(), state);
+            }
+        }
+    }
+}
---
+++ Funzionale/State Monads/Rws/RwsExtensions.cs
@@ -0,0 +1,83 @@
+﻿namespace Funzionale
+{
+    using static Prelude;
+
+    public static class RwsExtensions
+    {
+        // Functor
+
+        public static Rws<MonoidW, Env, W, S, R> Map<MonoidW, Env, W, S, T, R>(
+            this Rws<MonoidW, Env, W, S, T> @this, Func<T, R> map) where MonoidW : struct, Monoid<W> =>
+                new((env, state) => @this.func(env, state).Transform(t => (map(t.value), t.output, t.state)));
+
+        public static Rws<MonoidW, Env, W, S, Func<T2, R>> Map<MonoidW, Env, W, S, T1, T2, R>(
+            this Rws<MonoidW, Env, W, S, T1> @this, Func<T1, T2, R> map) where MonoidW : struct, Monoid<W> =>
+                @this.Map(map.Curry());
+
+        public static Rws<MonoidW, Env, W, S, Func<T2, T3, R>> Map<MonoidW, Env, W, S, T1, T2, T3, R>(
+            this Rws<MonoidW, Env, W, S, T1> @this, Func<T1, T2, T3, R> map) where MonoidW : struct, Monoid<W> =>
+                @this.Map(map.CurryFirst());
+
+        public static Rws<MonoidW, Env, W, S, Func<T2, T3, T4, R>> Map<MonoidW, Env, W, S, T1, T2, T3, T4, R>(
+            this Rws<MonoidW, Env, W, S, T1> @this, Func<T1, T2, T3, T4, R> map) where MonoidW : struct, Monoid<W> =>
+                @this.Map(map.CurryFirst());
+
+        public static Rws<MonoidW, Env, W, S, Func<T2, T3, T4, T5, R>> Map<MonoidW, Env, W, S, T1, T2, T3, T4, T5, R>(
+            this Rws<MonoidW, Env, W, S, T1> @this, Func<T1, T2, T3, T4, T5, R> map) where MonoidW : struct, Monoid<W> =>
+                @this.Map(map.CurryFirst());
+
+        // Monad
+
+        public static Rws<MonoidW, Env, W, S, R> Bind<MonoidW, Env, W, S, T, R>(
+            this Rws<MonoidW, Env, W, S, T> @this, Func<T, Rws<MonoidW, Env, W, S, R>> bind) where MonoidW : struct, Monoid<W> =>
+                new((env, state) => @this.func(env, state)
+                    .Transform(x => bind(x.value).func(env, x.state)
+                        .Transform(y => (y.value, concat<MonoidW, W>(x.output, y.output), y.state))));
+
+        // Applicative
+
+        public static Rws<MonoidW, Env, W, S, R> Apply<MonoidW, Env, W, S, T, R>(
+            this Rws<MonoidW, Env, W, S, Func<T, R>> @this, Rws<MonoidW, Env, W, S, T> arg) where MonoidW : struct, Monoid<W> =>
+                new((env, state) => @this.func(env, state)
+                    .Transform(x => arg.func(env, x.state)
+                        .Transform(y => (x.value(y.value), concat<MonoidW, W>(x.output, y.output), y.state))));
+
+        public static Rws<MonoidW, Env, W, S, Func<T2, R>> Apply<MonoidW, Env, W, S, T1, T2, R>(
+            this Rws<MonoidW, Env, W, S, Func<T1, T2, R>> @this, Rws<MonoidW, Env, W, S, T1> arg)
+                where MonoidW : struct, Monoid<W> =>
+                    @this.Map(FuncExtensions.CurryFirst).Apply(arg);
+
+        public static Rws<MonoidW, Env, W, S, Func<T2, T3, R>> Apply<MonoidW, Env, W, S, T1, T2, T3, R>(
+            this Rws<MonoidW, Env, W, S, Func<T1, T2, T3, R>> @this, Rws<MonoidW, Env, W, S, T1> arg) where MonoidW : struct, Monoid<W> =>
+                @this.Map(FuncExtensions.CurryFirst).Apply(arg);
+
+        public static Rws<MonoidW, Env, W, S, Func<T2, T3, T4, R>> Apply<MonoidW, Env, W, S, T1, T2, T3, T4, R>(
+            this Rws<MonoidW, Env, W, S, Func<T1, T2, T3, T4, R>> @this, Rws<MonoidW, Env, W, S, T1> arg) where MonoidW : struct, Monoid<W> =>
+                @this.Map(FuncExtensions.CurryFirst).Apply(arg);
+
+        public static Rws<MonoidW, Env, W, S, Func<T2, T3, T4, T5, R>> Apply<MonoidW, Env, W, S, T1, T2, T3, T4, T5, R>(
+            this Rws<MonoidW, Env, W, S, Func<T1, T2, T3, T4, T5, R>> @this, Rws<MonoidW, Env, W, S, T1> arg) where MonoidW : struct, Monoid<W> =>
+                @this.Map(FuncExtensions.CurryFirst).Apply(arg);
+
+        // Utilities
+
+        public static Rws<MonoidW, Env, W, S, Unit> ForEach<MonoidW, Env, W, S, T>(
+            this Rws<MonoidW, Env, W, S, T> @this, Action<T> action) where MonoidW : struct, Monoid<W> =>
+                @this.Map(action.ToFunc());
+
+        // LINQ
+
+        public static Rws<MonoidW, Env, W, S, R> Select<MonoidW, Env, W, S, T, R>(
+            this Rws<MonoidW, Env, W, S, T> @this, Func<T, R> map) where MonoidW : struct, Monoid<W> => @this.Map(map);
+
+        public static Rws<MonoidW, Env, W, S, R> SelectMany<MonoidW, Env, W, S, T, R>(
+            this Rws<MonoidW, Env, W, S, T> @this, Func<T, Rws<MonoidW, Env, W, S, R>> bind) where MonoidW : struct, Monoid<W> =>
+                @this.Bind(bind);
+
+        public static Rws<MonoidW, Env, W, S, V> SelectMany<MonoidW, Env, W, S, T, R, V>(
+            this Rws<MonoidW, Env, W, S, T> @this,
+            Func<T, Rws<MonoidW, Env, W, S, R>> bind,
+            Func<T, R, V> project) where MonoidW : struct, Monoid<W> =>
+                @this.Bind(t => bind(t).Map(r => project(t, r)));
+    }
+}
