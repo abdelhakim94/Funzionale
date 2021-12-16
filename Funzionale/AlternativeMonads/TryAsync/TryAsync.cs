8fadf0859da2fae749c0e42f5005cadcfbeed94a
Abdelhakim OUBRAHAM <abdel.oubraham@gmail.com>
2021-12-16 12:19:26 +0100

    Define TryAsync

---
+++ Funzionale/AlternativeMonads/TryAsync/TryAsync.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Funzionale
+{
+    using static Prelude;
+
+    public static partial class Prelude
+    {
+        public static TryAsync<T> tryAsync<T>([DisallowNull][NotNull] T value) => new(() => Task.FromResult(value));
+        public static TryAsync<T> tryAsync<T>([DisallowNull][NotNull] Func<Task<T>> f) => f;
+    }
+
+    public readonly struct TryAsync<T>
+    {
+        internal readonly Func<Task<T>> func;
+
+        internal TryAsync([DisallowNull][NotNull] Func<Task<T>> f) =>
+            func = f ?? throw new ArgumentNullException(nameof(f), "Cannot initialize a Try with a null delegate.");
+
+        public static implicit operator TryAsync<T>(Func<Task<T>> f) => new(f);
+
+        public async Task<Exceptional<T>> Run()
+        {
+            try
+            {
+                return success(await func() ?? throw new ArgumentNullException("The function provided to the Try monad returned null. Consider using an Option instead."));
+            }
+            catch (Exception ex)
+            {
+                return failure<T>(ex);
+            }
+        }
+    }
+}
---
+++ Funzionale/AlternativeMonads/TryAsync/TryAsyncExtensions.cs
@@ -0,0 +1,56 @@
+﻿namespace Funzionale
+{
+    public static class TryAsyncAsyncExtensions
+    {
+        // Functor
+
+        public static TryAsync<R> Map<T, R>(this TryAsync<T> @this, Func<T, R> map) => new(() => @this.func().Map(map));
+
+        public static TryAsync<Func<T2, R>> Map<T1, T2, R>(this TryAsync<T1> @this, Func<T1, T2, R> map) =>
+            @this.Map(map.Curry());
+
+        public static TryAsync<Func<T2, T3, R>> Map<T1, T2, T3, R>(this TryAsync<T1> @this, Func<T1, T2, T3, R> map) =>
+            @this.Map(map.CurryFirst());
+
+        public static TryAsync<Func<T2, T3, T4, R>> Map<T1, T2, T3, T4, R>(this TryAsync<T1> @this, Func<T1, T2, T3, T4, R> map) =>
+            @this.Map(map.CurryFirst());
+
+        public static TryAsync<Func<T2, T3, T4, T5, R>> Map<T1, T2, T3, T4, T5, R>(this TryAsync<T1> @this, Func<T1, T2, T3, T4, T5, R> map) =>
+            @this.Map(map.CurryFirst());
+
+        // Monad
+
+        public static TryAsync<R> Bind<T, R>(this TryAsync<T> @this, Func<T, TryAsync<R>> bind) =>
+            new(async () => await bind(await @this.func()).func());
+
+        // Apply
+
+        public static TryAsync<R> Apply<T, R>(this TryAsync<Func<T, R>> @this, TryAsync<T> arg) =>
+            new(async () => (await @this.func())(await arg.func()));
+
+        public static TryAsync<Func<T2, R>> Apply<T1, T2, R>(this TryAsync<Func<T1, T2, R>> @this, TryAsync<T1> arg) =>
+            @this.Map(FuncExtensions.CurryFirst).Apply(arg);
+
+        public static TryAsync<Func<T2, T3, R>> Apply<T1, T2, T3, R>(this TryAsync<Func<T1, T2, T3, R>> @this, TryAsync<T1> arg) =>
+            @this.Map(FuncExtensions.CurryFirst).Apply(arg);
+
+        public static TryAsync<Func<T2, T3, T4, R>> Apply<T1, T2, T3, T4, R>(this TryAsync<Func<T1, T2, T3, T4, R>> @this, TryAsync<T1> arg) =>
+            @this.Map(FuncExtensions.CurryFirst).Apply(arg);
+
+        public static TryAsync<Func<T2, T3, T4, T5, R>> Apply<T1, T2, T3, T4, T5, R>(this TryAsync<Func<T1, T2, T3, T4, T5, R>> @this, TryAsync<T1> arg) =>
+            @this.Map(FuncExtensions.CurryFirst).Apply(arg);
+
+        // Utilities
+
+        public static TryAsync<Unit> ForEach<T>(this TryAsync<T> @this, Action<T> action) => @this.Map(action.ToFunc());
+
+        // LINQ
+
+        public static TryAsync<R> Select<T, R>(this TryAsync<T> @this, Func<T, R> map) => @this.Map(map);
+
+        public static TryAsync<R> SelectMany<T, R>(this TryAsync<T> @this, Func<T, TryAsync<R>> bind) => @this.Bind(bind);
+
+        public static TryAsync<S> SelectMany<T, R, S>(this TryAsync<T> @this, Func<T, TryAsync<R>> bind, Func<T, R, S> project) =>
+            @this.Bind(t => bind(t).Map(r => project(t, r)));
+    }
+}
