57c08bea526700f988a1848a62b3baa281b19705
Abdelhakim OUBRAHAM <abdel.oubraham@gmail.com>
2021-12-16 09:57:23 +0100

    Define StateAsync

--- Funzionale/State Monads/StateAsync/StateAsync.cs
+++ Funzionale/State Monads/StateAsync/StateAsync.cs
@@ -5,28 +5,30 @@ namespace Funzionale
     using static Prelude;
     public static partial class Prelude
     {
-        public static State<S, T> state<S, T>([DisallowNull][NotNull] T value) => new(state => (value, state));
+        public static StateAsync<S, T> stateAsync<S, T>([DisallowNull][NotNull] T value) =>
+            new(state => Task.FromResult((value, state)));
     }
 
     /// <summary>
     /// A monad that produces a value T and changes state along the computation
     /// </summary>
-    public readonly struct State<S, T>
+    public readonly struct StateAsync<S, T>
     {
-        internal readonly Func<S, (T value, S state)> func;
-        internal State([DisallowNull][NotNull] Func<S, (T value, S state)> f) =>
+        internal readonly Func<S, Task<(T value, S state)>> func;
+
+        internal StateAsync([DisallowNull][NotNull] Func<S, Task<(T value, S state)>> f) =>
             func = f ?? throw new ArgumentNullException(nameof(f), "Cannot initialize a State monad with an empty delegate");
 
-        public static implicit operator State<S, T>([DisallowNull][NotNull] Func<S, (T value, S state)> f) => new(f);
+        public static implicit operator StateAsync<S, T>([DisallowNull][NotNull] Func<S, Task<(T value, S state)>> f) => new(f);
 
-        public (Exceptional<T> value, S state) Run(S state)
+        public async Task<(Exceptional<T> value, S state)> Run(S state)
         {
             try
             {
                 if (state is null)
                     throw new ArgumentNullException(nameof(state), "The initiale state provided to the State monad was null. Consider using an Option instead.");
 
-                var (value, newState) = func(state);
+                var (value, newState) = await func(state);
 
                 if (value is null)
                     throw new ArgumentNullException(nameof(value), "The function provided to the Writer returned a null value. Consider using an Option instead.");
---
+++ Funzionale/State Monads/StateAsync/StateAsyncExtensions.cs
@@ -0,0 +1,60 @@
+﻿namespace Funzionale
+{
+    public static partial class StateAsyncExtensions
+    {
+        // Functor
+
+        public static StateAsync<S, R> Map<T, R, S>(this StateAsync<S, T> @this, Func<T, R> map) =>
+            new(state => @this.func(state).Map(x => (map(x.value), x.state)));
+
+        public static StateAsync<S, Func<T2, R>> Map<T1, T2, R, S>(this StateAsync<S, T1> @this, Func<T1, T2, R> map) =>
+            @this.Map(map.Curry());
+
+        public static StateAsync<S, Func<T2, T3, R>> Map<T1, T2, T3, R, S>(this StateAsync<S, T1> @this, Func<T1, T2, T3, R> map) =>
+            @this.Map(map.CurryFirst());
+
+        public static StateAsync<S, Func<T2, T3, T4, R>> Map<T1, T2, T3, T4, R, S>(this StateAsync<S, T1> @this, Func<T1, T2, T3, T4, R> map) =>
+            @this.Map(map.CurryFirst());
+
+        public static StateAsync<S, Func<T2, T3, T4, T5, R>> Map<T1, T2, T3, T4, T5, R, S>(this StateAsync<S, T1> @this, Func<T1, T2, T3, T4, T5, R> map) =>
+            @this.Map(map.CurryFirst());
+
+        // Monad
+
+        public static StateAsync<S, R> Bind<T, R, S>(this StateAsync<S, T> @this, Func<T, StateAsync<S, R>> bind) =>
+            new(state => @this.func(state).Map(x => bind(x.value).func(x.state)).Unwrap());
+
+        // Applicative
+
+        public static StateAsync<S, R> Apply<T, R, S>(this StateAsync<S, Func<T, R>> @this, StateAsync<S, T> arg) =>
+            new(state => @this.func(state)
+                .Map(x => arg.func(x.state)
+                    .Map(y => (x.value(y.value), y.state)))
+            .Unwrap());
+
+        public static StateAsync<S, Func<T2, R>> Apply<T1, T2, R, S>(this StateAsync<S, Func<T1, T2, R>> @this, StateAsync<S, T1> arg) =>
+            @this.Map(FuncExtensions.Curry).Apply(arg);
+
+        public static StateAsync<S, Func<T2, T3, R>> Apply<T1, T2, T3, R, S>(this StateAsync<S, Func<T1, T2, T3, R>> @this, StateAsync<S, T1> arg) =>
+            @this.Map(FuncExtensions.CurryFirst).Apply(arg);
+
+        public static StateAsync<S, Func<T2, T3, T4, R>> Apply<T1, T2, T3, T4, R, S>(this StateAsync<S, Func<T1, T2, T3, T4, R>> @this, StateAsync<S, T1> arg) =>
+            @this.Map(FuncExtensions.CurryFirst).Apply(arg);
+
+        public static StateAsync<S, Func<T2, T3, T4, T5, R>> Apply<T1, T2, T3, T4, T5, R, S>(this StateAsync<S, Func<T1, T2, T3, T4, T5, R>> @this, StateAsync<S, T1> arg) =>
+            @this.Map(FuncExtensions.CurryFirst).Apply(arg);
+
+        // Utilities
+
+        public static StateAsync<S, Unit> ForEach<T, S>(this StateAsync<S, T> @this, Action<T> action) => @this.Map(action.ToFunc());
+
+        // LINQ
+
+        public static StateAsync<S, R> Select<T, R, S>(this StateAsync<S, T> @this, Func<T, R> map) => @this.Map(map);
+
+        public static StateAsync<S, R> SelectMany<T, R, S>(this StateAsync<S, T> @this, Func<T, StateAsync<S, R>> bind) => @this.Bind(bind);
+
+        public static StateAsync<S, V> SelectMany<T, R, V, S>(this StateAsync<S, T> @this, Func<T, StateAsync<S, R>> bind, Func<T, R, V> project) =>
+                @this.Bind(t => bind(t).Map(r => project(t, r)));
+    }
+}
