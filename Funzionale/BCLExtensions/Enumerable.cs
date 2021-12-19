8747279c25217816c74af1b66b6393113b9b9b5b
Abdelhakim OUBRAHAM <abdel.oubraham@gmail.com>
2021-12-19 17:55:35 +0100

    Make IEnumerable traversable with Reader, Writer, State and Rws monads

--- Funzionale/BCLExtensions/Enumerable.cs
+++ Funzionale/BCLExtensions/Enumerable.cs
@@ -108,5 +108,180 @@
                                         .Apply(taskRs)
                                         .Apply(f(t)));
 
+        /// <summary>
+        /// IEnumerable to Reader traverse with the monadic flow.
+        /// </summary>
+        public static Reader<Env, IEnumerable<R>> TraverseM<Env, T, R>(this IEnumerable<T> @this, Func<T, Reader<Env, R>> f)
+            where Env : struct =>
+                @this.Aggregate(
+                    seed: reader<Env, IEnumerable<R>>(Enumerable.Empty<R>()),
+                    func: (readRs, t) => from rs in readRs
+                                         from r in f(t)
+                                         select rs.Append(r));
+
+        /// <summary>
+        /// IEnumerable to Reader traverse with the applicative flow.
+        /// </summary>
+        public static Reader<Env, IEnumerable<R>> TraverseA<Env, T, R>(this IEnumerable<T> @this, Func<T, Reader<Env, R>> f)
+            where Env : struct =>
+                @this.Aggregate(
+                    seed: reader<Env, IEnumerable<R>>(Enumerable.Empty<R>()),
+                    func: (readRs, t) => reader<Env, Func<IEnumerable<R>, R, IEnumerable<R>>>(Enumerable.Append<R>)
+                                            .Apply(readRs)
+                                            .Apply(f(t)));
+
+        /// <summary>
+        /// IEnumerable to ReaderAsync traverse with the monadic flow.
+        /// </summary>
+        public static ReaderAsync<Env, IEnumerable<R>> TraverseM<Env, T, R>(this IEnumerable<T> @this, Func<T, ReaderAsync<Env, R>> f)
+            where Env : struct =>
+                @this.Aggregate(
+                    seed: readerAsync<Env, IEnumerable<R>>(Enumerable.Empty<R>()),
+                    func: (readRs, t) => from rs in readRs
+                                         from r in f(t)
+                                         select rs.Append(r));
+
+        /// <summary>
+        /// IEnumerable to ReaderAsync traverse with the applicative flow.
+        /// </summary>
+        public static ReaderAsync<Env, IEnumerable<R>> TraverseA<Env, T, R>(this IEnumerable<T> @this, Func<T, ReaderAsync<Env, R>> f)
+            where Env : struct =>
+                @this.Aggregate(
+                    seed: readerAsync<Env, IEnumerable<R>>(Enumerable.Empty<R>()),
+                    func: (readRs, t) => readerAsync<Env, Func<IEnumerable<R>, R, IEnumerable<R>>>(Enumerable.Append<R>)
+                                            .Apply(readRs)
+                                            .Apply(f(t)));
+
+        /// <summary>
+        /// IEnumerable to Writer traverse with the monadic flow.
+        /// </summary>
+        public static Writer<MonoidW, W, IEnumerable<R>> TraverseM<MonoidW, W, T, R>(this IEnumerable<T> @this, Func<T, Writer<MonoidW, W, R>> f)
+            where MonoidW : struct, Monoid<W> =>
+                @this.Aggregate(
+                    seed: writer<MonoidW, W, IEnumerable<R>>(Enumerable.Empty<R>()),
+                    func: (writRs, t) => from rs in writRs
+                                         from r in f(t)
+                                         select rs.Append(r));
+
+        /// <summary>
+        /// IEnumerable to Writer traverse with the applicative flow.
+        /// </summary>
+        public static Writer<MonoidW, W, IEnumerable<R>> TraverseA<MonoidW, W, T, R>(this IEnumerable<T> @this, Func<T, Writer<MonoidW, W, R>> f)
+            where MonoidW : struct, Monoid<W> =>
+                @this.Aggregate(
+                    seed: writer<MonoidW, W, IEnumerable<R>>(Enumerable.Empty<R>()),
+                    func: (writRs, t) => writer<MonoidW, W, Func<IEnumerable<R>, R, IEnumerable<R>>>(Enumerable.Append<R>)
+                                            .Apply(writRs)
+                                            .Apply(f(t)));
+
+        /// <summary>
+        /// IEnumerable to WriterAsync traverse with the monadic flow.
+        /// </summary>
+        public static WriterAsync<MonoidW, W, IEnumerable<R>> TraverseM<MonoidW, W, T, R>(this IEnumerable<T> @this, Func<T, WriterAsync<MonoidW, W, R>> f)
+            where MonoidW : struct, Monoid<W> =>
+                @this.Aggregate(
+                    seed: writerAsync<MonoidW, W, IEnumerable<R>>(Enumerable.Empty<R>()),
+                    func: (writRs, t) => from rs in writRs
+                                         from r in f(t)
+                                         select rs.Append(r));
+
+        /// <summary>
+        /// IEnumerable to WriterAsync traverse with the applicative flow.
+        /// </summary>
+        public static WriterAsync<MonoidW, W, IEnumerable<R>> TraverseA<MonoidW, W, T, R>(this IEnumerable<T> @this, Func<T, WriterAsync<MonoidW, W, R>> f)
+            where MonoidW : struct, Monoid<W> =>
+                @this.Aggregate(
+                    seed: writerAsync<MonoidW, W, IEnumerable<R>>(Enumerable.Empty<R>()),
+                    func: (writRs, t) => writerAsync<MonoidW, W, Func<IEnumerable<R>, R, IEnumerable<R>>>(Enumerable.Append<R>)
+                                            .Apply(writRs)
+                                            .Apply(f(t)));
+
+        /// <summary>
+        /// IEnumerable to State traverse with the monadic flow.
+        /// </summary>
+        public static State<S, IEnumerable<R>> TraverseM<S, T, R>(this IEnumerable<T> @this, Func<T, State<S, R>> f) =>
+                @this.Aggregate(
+                    seed: state<S, IEnumerable<R>>(Enumerable.Empty<R>()),
+                    func: (statRs, t) => from rs in statRs
+                                         from r in f(t)
+                                         select rs.Append(r));
+
+        /// <summary>
+        /// IEnumerable to State traverse with the applicative flow.
+        /// </summary>
+        public static State<S, IEnumerable<R>> TraverseA<S, T, R>(this IEnumerable<T> @this, Func<T, State<S, R>> f) =>
+                @this.Aggregate(
+                    seed: state<S, IEnumerable<R>>(Enumerable.Empty<R>()),
+                    func: (statRs, t) => state<S, Func<IEnumerable<R>, R, IEnumerable<R>>>(Enumerable.Append<R>)
+                                            .Apply(statRs)
+                                            .Apply(f(t)));
+
+        /// <summary>
+        /// IEnumerable to StateAsync traverse with the monadic flow.
+        /// </summary>
+        public static StateAsync<S, IEnumerable<R>> TraverseM<S, T, R>(this IEnumerable<T> @this, Func<T, StateAsync<S, R>> f) =>
+                @this.Aggregate(
+                    seed: stateAsync<S, IEnumerable<R>>(Enumerable.Empty<R>()),
+                    func: (statRs, t) => from rs in statRs
+                                         from r in f(t)
+                                         select rs.Append(r));
+
+        /// <summary>
+        /// IEnumerable to StateAsync traverse with the applicative flow.
+        /// </summary>
+        public static StateAsync<S, IEnumerable<R>> TraverseA<S, T, R>(this IEnumerable<T> @this, Func<T, StateAsync<S, R>> f) =>
+                @this.Aggregate(
+                    seed: stateAsync<S, IEnumerable<R>>(Enumerable.Empty<R>()),
+                    func: (statRs, t) => stateAsync<S, Func<IEnumerable<R>, R, IEnumerable<R>>>(Enumerable.Append<R>)
+                                            .Apply(statRs)
+                                            .Apply(f(t)));
+
+        /// <summary>
+        /// IEnumerable to Rws traverse with the monadic flow.
+        /// </summary>
+        public static Rws<MonoidW, Env, W, S, IEnumerable<R>> TraverseM<MonoidW, Env, W, S, T, R>(this IEnumerable<T> @this, Func<T, Rws<MonoidW, Env, W, S, R>> f)
+           where Env : struct
+           where MonoidW : struct, Monoid<W> =>
+               @this.Aggregate(
+                    seed: rws<MonoidW, Env, W, S, IEnumerable<R>>(Enumerable.Empty<R>()),
+                    func: (rwsRs, t) => from rs in rwsRs
+                                        from r in f(t)
+                                        select rs.Append(r));
+
+        /// <summary>
+        /// IEnumerable to Rws traverse with the applicative flow.
+        /// </summary>
+        public static Rws<MonoidW, Env, W, S, IEnumerable<R>> TraverseA<MonoidW, Env, W, S, T, R>(this IEnumerable<T> @this, Func<T, Rws<MonoidW, Env, W, S, R>> f)
+            where Env : struct
+            where MonoidW : struct, Monoid<W> =>
+                @this.Aggregate(
+                    seed: rws<MonoidW, Env, W, S, IEnumerable<R>>(Enumerable.Empty<R>()),
+                    func: (rwsRs, t) => rws<MonoidW, Env, W, S, Func<IEnumerable<R>, R, IEnumerable<R>>>(Enumerable.Append<R>)
+                                            .Apply(rwsRs)
+                                            .Apply(f(t)));
+
+        /// <summary>
+        /// IEnumerable to RwsAsync traverse with the monadic flow.
+        /// </summary>
+        public static RwsAsync<MonoidW, Env, W, S, IEnumerable<R>> TraverseM<MonoidW, Env, W, S, T, R>(this IEnumerable<T> @this, Func<T, RwsAsync<MonoidW, Env, W, S, R>> f)
+           where Env : struct
+           where MonoidW : struct, Monoid<W> =>
+               @this.Aggregate(
+                    seed: rwsAsync<MonoidW, Env, W, S, IEnumerable<R>>(Enumerable.Empty<R>()),
+                    func: (rwsRs, t) => from rs in rwsRs
+                                        from r in f(t)
+                                        select rs.Append(r));
+
+        /// <summary>
+        /// IEnumerable to RwsAsync traverse with the applicative flow.
+        /// </summary>
+        public static RwsAsync<MonoidW, Env, W, S, IEnumerable<R>> TraverseA<MonoidW, Env, W, S, T, R>(this IEnumerable<T> @this, Func<T, RwsAsync<MonoidW, Env, W, S, R>> f)
+            where Env : struct
+            where MonoidW : struct, Monoid<W> =>
+                @this.Aggregate(
+                    seed: rwsAsync<MonoidW, Env, W, S, IEnumerable<R>>(Enumerable.Empty<R>()),
+                    func: (rwsRs, t) => rwsAsync<MonoidW, Env, W, S, Func<IEnumerable<R>, R, IEnumerable<R>>>(Enumerable.Append<R>)
+                                            .Apply(rwsRs)
+                                            .Apply(f(t)));
     }
 }
